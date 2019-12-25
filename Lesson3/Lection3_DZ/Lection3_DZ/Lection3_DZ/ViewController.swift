e76f747161f73481ed02ac3146203f0b548c0816
tommyvercetti <andriancrick@gmail.com>
2019-12-25 15:30:55 +0200

    lesson 3 finished

--- Lesson3/Lection3_DZ/Lection3_DZ/Lection3_DZ/ViewController.swift
+++ Lesson3/Lection3_DZ/Lection3_DZ/Lection3_DZ/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //С помощью кода drawBox см листинг с лекции, нарисовать цель и пирамиду из кубиков  
+        //С помощью кода drawBox см листинг с лекции, нарисовать цель и пирамиду из кубиков       
         
         drawPyramid(ammountOfBoxes: 10)
         
