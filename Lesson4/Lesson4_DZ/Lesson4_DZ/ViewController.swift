ad8b435aabb4117a79cfd1322337ed4c8f115230
tommyvercetti <andriancrick@gmail.com>
2019-12-26 12:23:35 +0200

    lesson 4 code DZ start

---
+++ Lesson4/Lesson4_DZ/Lesson4_DZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson4_DZ
+//
+//  Created by Andrian Kryk on 26.12.2019.
+//  Copyright © 2019 Andrian Kryk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
